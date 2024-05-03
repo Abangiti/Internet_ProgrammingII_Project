b2077a0f9248cb891ce2e6bd42bf989e1ffe8407
salemhabte <habtesalem20@gmail.com>
2024-05-03 08:14:31 -0700

    change

---
+++ LinkdE/linkedE/src/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Pages/Login'
+import Home from './Pages/Home'
+import Signup from './Pages/Signup'
+import Aboutus from './Pages/Aboutus'
+import Birthday from './Pages/Birthday'
+import Concert from './Pages/Concert'
+
+
+
+// import Navbar from './Pages/Navbar'
+import Contactus from './Pages/Contactus'
+import Event from './Pages/Event';
+// import Footer from './Pages/Footer';
+
+import FAQ from './Pages/FAQ';
+import Funeral from './Pages/Funeral';
+import Graduation from './Pages/Graduation';
+// import Service from './Pages/Service';
+import Wedding from './Pages/Wedding';
+
+function App() {
+
+  return (
+  
+    <BrowserRouter>
+    {/* <Navigation/> */}
+    
+    
+      <Routes>
+      
+        <Route path="/" element={<Home />} />
+        <Route path='/Aboutus' element= {<Aboutus/>}/>
+                 <Route path='/Birthday' element= {<Birthday/>}/>
+                <Route path='/Concert' element= {<Concert/>}/>
+                 <Route path='/Funera' element={<Funeral/>}/>
+                <Route path='/Graduation ' element={<Graduation />}/>
+               <Route path='/Wedding' element={<Wedding/>}/>
+                <Route path='/Event' element={<Event/>}/>
+                 <Route path='/Contactus' element={<Contactus/>}/>
+               <Route path='/Login' element={<Login/>}/>
+               <Route path='/FAQ' element={<FAQ/>}/>
+               <Route path='/signup' element={<Signup/>}/>
+      </Routes>
+      {/* <Footer/> */}
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ LinkdE/linkedE/src/Pages/Signup.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function Signup() {
+  return (
+    <main>
+    <form id="customersignup" onsubmit="return validateForm()">
+      <h2>Sign Up</h2>
+      <div className="name-group">
+        <label htmlFor="firstname">First Name: </label>
+        <input type="text" id="firstname" name="firstname" required="" />
+        <label htmlFor="lastname" className="last">
+          Last Name:{" "}
+        </label>
+        <input type="text" id="lastname" name="lastname" required="" />
+      </div>
+      <label htmlFor="birthdate">Birthdate:</label>
+      <input type="date" id="birthdate" name="birthdate" required="" />
+      <label htmlFor="phonenumber">Phone Number:</label>
+      <input
+        type="tel"
+        id="phonenumber"
+        name="phonenumber"
+        maxLength={13}
+        required=""
+      />
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" required="" />
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" name="password" required="" />
+      <div id="sError" className="error-message" />
+      <label htmlFor="confirmPassword">Confirm Password:</label>
+      <input
+        type="password"
+        id="confirmPassword"
+        name="confirmPassword"
+        required=""
+      />
+      <div id="passwordError" className="error-message" />
+      <input type="submit" defaultValue="Register" />
+    </form>
+  </main>
+  
+  )
+}
